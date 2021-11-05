760df9338f1c18d43b8c528a77281395b25e384d
Berk Bavaş <berkbavas@gmail.com>
2021-11-06 00:42:58 +0300

    "XML" folder is renamed as "Data".

--- Constants.h
+++ Constants.h
@@ -9,7 +9,7 @@ const int SCALE_FACTOR  = 4; // 2^x
 const int CURVE_THICKNESS  = 2 * SCALE_FACTOR; // 2^x
 const float STEP  = 0.0001f;
 
-const QString XML_DIR_NAME = QString("XML");
+const QString XML_DIR_NAME = QString("Data");
 const QDir XML_DIR = QDir(XML_DIR_NAME);
 
 const QString OUTPUT_DIR_NAME = "Output";
--- DiffusionCurvesPoC.pro
+++ DiffusionCurvesPoC.pro
@@ -3,7 +3,7 @@ QT += gui xml
 CONFIG += c++11 console
 CONFIG -= app_bundle
 
-DEFINES += DEBUG
+DEFINES -= DEBUG
 
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
